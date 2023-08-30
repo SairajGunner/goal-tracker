9e3276b802fdb56cf5162a5f7713189bb1d23043
Sairaj Udayshankar <sairaj9396@gmail.com>
2023-08-30 19:23:18 -0400

    App layout and styling

---
+++ goal-tracker-app/src/app/shared/components/goal-card/goal-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-card',
+  templateUrl: './goal-card.component.html',
+  styleUrls: ['./goal-card.component.scss']
+})
+export class GoalCardComponent {
+
+}
